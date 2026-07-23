7da7981d3c6af13f60fa35a9ee512755956c0a59
Leonardo Razovic <lrazovic@gmail.com>
2026-07-23 15:35:09 +0200

    chore: remvoe the assetId configuration types

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -3535,8 +3535,6 @@ parameter_types! {
 }
 
 impl pallet_stability::Config for Runtime {
-	type CollateralAssetId = VaultsCollateralId;
-	type StableAssetId = VaultsStableId;
 	type StableAssets = Assets;
 	type CollateralAssets = StabilityCollateral;
 	type TimeProvider = Timestamp;
--- substrate/frame/stability/src/pending.rs
+++ substrate/frame/stability/src/pending.rs
@@ -2,22 +2,22 @@
 //! instance (`StableListId::StabilityPending`); ordering discipline as per
 //! [`fifo_append`], the same convention as vaults' `FinalRecovery` FIFO.
 
-use crate::pallet::{Config, Error};
+use crate::pallet::{CollateralIdOf, Config, Error, StableIdOf};
 use frame::prelude::*;
 use pallet_linked_list::{fifo_append, SortedListInterface};
 use pusd_primitives::StableListId;
 
 /// The per-branch FIFO list id.
 pub(crate) fn list_id<T: Config>(
-	collateral_id: &T::CollateralAssetId,
-	stable_id: &T::StableAssetId,
-) -> StableListId<T::CollateralAssetId, T::StableAssetId> {
+	collateral_id: &CollateralIdOf<T>,
+	stable_id: &StableIdOf<T>,
+) -> StableListId<CollateralIdOf<T>, StableIdOf<T>> {
 	StableListId::StabilityPending(collateral_id.clone(), stable_id.clone())
 }
 
 /// Append `depositor` to the `fifo` list. Errors if already present.
 pub(crate) fn append<T: Config>(
-	fifo: &StableListId<T::CollateralAssetId, T::StableAssetId>,
+	fifo: &StableListId<CollateralIdOf<T>, StableIdOf<T>>,
 	depositor: T::AccountId,
 ) -> Result<(), DispatchError> {
 	fifo_append::<_, _, T::PendingLists>(fifo.clone(), depositor)
@@ -29,7 +29,7 @@ pub(crate) fn append<T: Config>(
 /// take the prebuilt list id ([`list_id`]) so loops do not reclone the asset
 /// ids per call.
 pub(crate) fn remove<T: Config>(
-	fifo: &StableListId<T::CollateralAssetId, T::StableAssetId>,
+	fifo: &StableListId<CollateralIdOf<T>, StableIdOf<T>>,
 	depositor: &T::AccountId,
 ) -> Result<(), DispatchError> {
 	T::PendingLists::remove(fifo, depositor).map_err(|_| Error::<T>::PendingFifoInvariantBroken)?;
