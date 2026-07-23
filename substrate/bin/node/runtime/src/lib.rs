c7ff38d83537519e9fbe6466247ffdcdf967f9ae
Leonardo Razovic <lrazovic@gmail.com>
2026-07-23 15:35:09 +0200

    chore: remvoe the assetId configuration types

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -3460,8 +3460,6 @@ parameter_types! {
 }
 
 impl pallet_stability::Config for Runtime {
-	type CollateralAssetId = VaultsCollateralId;
-	type StableAssetId = VaultsStableId;
 	type StableAssets = Assets;
 	type CollateralAssets = StabilityCollateral;
 	type TimeProvider = Timestamp;
